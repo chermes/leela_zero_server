63c315aa597fe4ee83325addec995d04b71e5165
chermes <hermes@hausmilbe.net>
2019-05-05 20:33:45 +0200

    added an empty worker script to the lz_worker service

---
+++ lz_worker/src/worker.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Small worker script which checks for new available Go games and runs the Leela
+Zero analysis if not already done so.
+"""
+import time
+
+import requests
+
+if __name__ == "__main__":
+    #  TODO: still needs to be done <05-05-19, Christoph Hermes> # 
+    while True:
+        time.sleep(1)
