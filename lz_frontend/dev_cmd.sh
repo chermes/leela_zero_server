263bf8fc2afd62be1168d9cdf70adfd571652a1e
chermes <hermes@hausmilbe.net>
2019-05-16 22:26:27 +0200

    added the files generated by the "vue create" command

--- lz_frontend/dev_cmd.sh
+++ lz_frontend/dev_cmd.sh
@@ -1,3 +1,13 @@
+# Run the dev container in this directory
+#
+# Created a new Vue.js project via
+# vue create --default -m npm --no-git lz_frontend
+# Afterwards, copy the newly generated lz_frontend/ contents to this level and
+# allowed the dev container Dockerfile.dev to install its dependencies.
+#
+# Run the service in a development mode:
+# npm run serve
+
 PWD=`pwd`
 
-docker run --rm -it -v $PWD:/src lz_dev:latest /bin/bash
+docker run --rm -it -v $PWD:/src -p 8080:8080 lz_dev:latest /bin/bash
