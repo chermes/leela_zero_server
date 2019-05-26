14878426bde260b60e3cc2793995ecc5a823cbf7
chermes <hermes@hausmilbe.net>
2019-05-26 23:50:24 +0200

    added some basic menu

--- lz_frontend/src/App.vue
+++ lz_frontend/src/App.vue
@@ -2,8 +2,30 @@
 <template>
   <b-container>
     <b-jumbotron header="Go Analysis Server" lead="Use Leela Zero to evaluate Go / Baduk / Weiqi games.">
-      <!-- <p>For more information visit our website</p> -->
-      <b-btn variant="primary" href="https://github.com/chermes/leela_zero_server/">GitHub Page</b-btn>
+      <b-button-toolbar>
+        <b-dropdown variant="primary" class="mx-1" right text="Upload Game">
+          <b-dropdown-item>SGF File</b-dropdown-item>
+          <b-dropdown-item>SGF Raw String</b-dropdown-item>
+          <b-dropdown-divider></b-dropdown-divider>
+          <b-dropdown-item>OGS ID</b-dropdown-item>
+          <b-dropdown-item>OGS Link</b-dropdown-item>
+          <b-dropdown-item>OGS Username</b-dropdown-item>
+        </b-dropdown>
+        <b-button-group class="mx-1">
+          <b-button variant="outline-primary">
+            <b-form-checkbox switch>LeelaZero Training</b-form-checkbox>
+          </b-button>
+        </b-button-group>
+        <b-button-group class="mx-1">
+          <b-dropdown variant="primary" class="mx-1" right text="References">
+            <b-dropdown-item href="https://github.com/chermes/leela_zero_server/">GitHub Project Page</b-dropdown-item>
+            <b-dropdown-item>API Documentation</b-dropdown-item>
+          <b-dropdown-divider></b-dropdown-divider>
+            <b-dropdown-item>Leela Zero Project</b-dropdown-item>
+            <b-dropdown-item>SGF Analyzer Project</b-dropdown-item>
+          </b-dropdown>
+        </b-button-group>
+      </b-button-toolbar>
     </b-jumbotron>
 
     <b-form-group horizontal :label-cols="4" description="Let us know your name." label="Enter your name">
